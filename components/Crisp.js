28e53e005be399020a8df8e9257374bb1e03fb8b
MaestroOG <muneeburrehman822@gmail.com>
2025-08-20 15:17:02 +0500

    crisp chat

---
+++ components/Crisp.js
@@ -0,0 +1,13 @@
+"use client";
+import { useEffect } from "react";
+import { Crisp } from "crisp-sdk-web";
+
+const CrispChat = () => {
+    useEffect(() => {
+        Crisp.configure(String(process.env.CRISP_WEBSITE_ID));
+    }, []);
+
+    return null;
+};
+
+export default CrispChat;

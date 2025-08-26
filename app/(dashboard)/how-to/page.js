a3296b68cfd9e9b702866a889fc4cb1df0ac9f74
MaestroOG <muneeburrehman822@gmail.com>
2025-08-26 07:28:07 +0500

    update pricelist pdf and add first how-to video

--- app/(dashboard)/how-to/page.js
+++ app/(dashboard)/how-to/page.js
@@ -1,8 +1,27 @@
+import Container from '@/components/dashboardComponents/Container'
 import React from 'react'
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 const HowToPage = () => {
     return (
-        <div>HowToPage</div>
+        <Container className={'bg-white p-4 grid grid-cols-1 md:grid-cols-3 gap-4'}>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Introducing the Stratital Agency Portal A Step by Step Guide</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <iframe width="360" height="315" src="https://www.youtube.com/embed/WzjoTJP4SPA?si=MJXBA7SZfbng0xtQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </CardContent>
+            </Card>
+        </Container>
     )
 }
 

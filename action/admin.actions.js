02ac30bdda892c9a7de082254021210e6e7555b2
MaestroOG <muneeburrehman822@gmail.com>
2025-09-30 21:58:47 +0500

    file upload err handling

--- action/admin.actions.js
+++ action/admin.actions.js
@@ -267,7 +267,7 @@ export async function editResource(prevState, formData) {
             updateData.fileUrl = await uploadFilesToCloudinary(file);
         } catch (err) {
             console.error(err);
-            if (error?.response?.status === 413) {
+            if (err?.response?.status === 413) {
                 return {
                     success: false,
                     message: "File is too large. Please upload a smaller file.",

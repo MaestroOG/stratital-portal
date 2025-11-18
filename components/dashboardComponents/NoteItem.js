d5ff415914d7aff2793463d31c1f43724ecd6a62
MaestroOG <muneeburrehman822@gmail.com>
2025-11-18 15:24:00 +0500

    qa: comment text overflow fix

--- components/dashboardComponents/NoteItem.js
+++ components/dashboardComponents/NoteItem.js
@@ -77,7 +77,7 @@ const NoteItem = ({ note, isUnread, onMarkAsRead, user }) => {
                 </Popover>}
             </div>
 
-            <div className="font-montserrat max-w-full w-full text-base sm:text-lg ml-0 sm:ml-11 font-medium prose prose-a:text-blue-500 prose-a:underline text-content break-words whitespace-pre-wrapoverflow-hidden"
+            <div className="font-montserrat md:max-w-5xl w-full text-base sm:text-lg ml-0 sm:ml-11 font-medium text-content break-words whitespace-pre-wrapoverflow-hidden"
             >
                 {parse(note?.note)}
             </div>
